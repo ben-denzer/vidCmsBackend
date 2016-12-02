ee2f1af2a2d87cec4e7539616a9a7f13073504ea
Ben Denzer <denzer.ben@gmail.com>
2016-12-01 22:51:59 -0800

    sending dates back with comments

--- src/routes/publicRouter.js
+++ src/routes/publicRouter.js
@@ -27,7 +27,7 @@ const router = (connection) => {
     publicRouter.post('/getComments', jsonParser, (req, res) => {
         console.log(req.body.video_id, 'id');
         connection.query(
-            'SELECT u.username, c.comment_text FROM comments c JOIN users u ON c.user_fk=u.user_id JOIN videos v ON c.video_fk=video_id WHERE c.video_fk=?',
+            'SELECT u.username, c.comment_text, c.comment_date FROM comments c JOIN users u ON c.user_fk=u.user_id JOIN videos v ON c.video_fk=video_id WHERE c.video_fk=?',
             [req.body.video_id],
             (err, rows) => {
                 if (err) return res.status(500).send({error: 'db error'});
