43bc1610cd1da0856ddf395c3da721daa5e2a810
Ben Denzer <denzer.ben@gmail.com>
2016-12-17 03:46:23 -0800

    removed logging in getAdminData

--- src/services/getAdminData.js
+++ src/services/getAdminData.js
@@ -9,13 +9,11 @@ const getAdminData = (connection, cb) => {
             connection.query('SELECT v.video_id, v.video_title, v.video_headline, v.premium, v.video_url,'
                 + 'v.video_text, v.video_date FROM videos v',
                 (err, rows) => {
-                    console.log('err2', err);
                     if (err) return cb({error: 'db error'});
                     allData.videos = rows;
 
                     connection.query('SELECT c.comment_id, c.comment_text, c.user_fk, c.video_fk, c.comment_date FROM comments c',
                         (err, rows) => {
-                            console.log('err3', err);
                             if (err) return cb({error: 'db error'});
                             allData.comments = rows;
                             cb(null, allData);
