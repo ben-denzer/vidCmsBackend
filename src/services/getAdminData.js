fa6391503f5000b2fe8ca25f4fbe34784a2d03c3
Ben Denzer <denzer.ben@gmail.com>
2017-01-24 22:13:09 -0800

    moved from callbacks to promises for admin data

--- src/services/getAdminData.js
+++ src/services/getAdminData.js
@@ -1,37 +1,33 @@
 const getAdminData = (connection, cb) => {
-    let allData = {};
-    connection.query('SELECT u.user_id, u.username, u.email, u.admin, u.premium, u.signup_date,'
-        + 'u.premium_signup_date, u.premium_expiration_date FROM users u',
-        (err, rows) => {
-            if (err) return cb({error: 'db error'});
-            allData.users = rows;
 
-            connection.query('SELECT v.video_id, v.video_title, v.video_headline, v.premium, v.video_url,'
-                + 'v.video_text, v.video_date FROM videos v',
-                (err, rows) => {
-                    if (err) return cb({error: 'db error'});
-                    allData.videos = rows;
+    const dbPromise = (query) => {
+        return new Promise((resolve, reject) => {
+            connection.query(query, (err, rows) => {
+                if (err) reject({error: 'db error'});
+                resolve(rows);
+            });
+        });
+    };
 
-                    connection.query('SELECT c.comment_id, c.comment_text, c.user_fk, c.video_fk, c.comment_date FROM comments c',
-                        (err, rows) => {
-                            if (err) return cb({error: 'db error'});
-                            allData.comments = rows;
+    const users = dbPromise('SELECT u.user_id, u.username, u.email, u.admin, u.premium, u.signup_date, u.premium_signup_date, u.premium_expiration_date FROM users u');
 
-                            connection.query('SELECT b.blog_id, b.blog_title, b.blog_headline, b.blog_text,'
-                                + 'b.blog_text, b.blog_post_url FROM blogs b',
-                                (err, rows) => {
-                                    if (err) return cb({error: 'db error'});
-                                    allData.blogs = rows;
+    const videos = dbPromise('SELECT v.video_id, v.video_title, v.video_headline, v.premium, v.video_url, v.video_text, v.video_date FROM videos v');
 
-                                    cb(null, allData);
-                                }
-                            );
-                        }
-                    );
-                }
-            );
-        }
-    );
+    const comments = dbPromise('SELECT c.comment_id, c.comment_text, c.user_fk, c.video_fk, c.comment_date FROM comments c');
+
+    const blogs = dbPromise('SELECT b.blog_id, b.blog_title, b.blog_headline, b.blog_text, b.blog_text, b.blog_post_url FROM blogs b');
+
+    const images = dbPromise('SELECT i.image_id, i.blog_fk, i.image_url FROM images i');
+
+    Promise.all([users, videos, comments, blogs, images])
+        .then(data => cb(null, Object.assign(
+            {},
+            {users      : data[0]},
+            {videos     : data[1]},
+            {comments   : data[2]},
+            {blogs      : data[3]},
+            {images     : data[4]}
+        ))).catch(() => cb({error: 'db error'}));
 };
 
 module.exports = getAdminData;
