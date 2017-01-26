784a3979b8625785e3f6fb7f57bb2327f208dd1a
Ben Denzer <denzer.ben@gmail.com>
2017-01-25 21:53:03 -0800

    refactored getAdminData a little more

--- src/services/getAdminData.js
+++ src/services/getAdminData.js
@@ -9,25 +9,29 @@ const getAdminData = (connection, cb) => {
         });
     };
 
-    const users = dbPromise('SELECT u.user_id, u.username, u.email, u.admin, u.premium, u.signup_date, u.premium_signup_date, u.premium_expiration_date FROM users u');
+    const getUsers = dbPromise(
+        'SELECT u.user_id, u.username, u.email, u.admin, u.premium, u.signup_date,'
+        + 'u.premium_signup_date, u.premium_expiration_date FROM users u'
+    );
+    const getVideos = dbPromise(
+        'SELECT v.video_id, v.video_title, v.video_headline, v.premium, v.video_url,'
+        + 'v.video_text, v.video_date FROM videos v'
+    );
+    const getComments = dbPromise(
+        'SELECT c.comment_id, c.comment_text, c.user_fk, c.video_fk, c.comment_date FROM comments c'
+    );
+    const getBlogs = dbPromise(
+        'SELECT b.blog_id, b.blog_title, b.blog_headline, b.blog_text, b.blog_text, b.blog_post_url FROM blogs b'
+    );
+    const getImages = dbPromise(
+        'SELECT i.image_id, i.blog_fk, i.image_url FROM images i'
+    );
 
-    const videos = dbPromise('SELECT v.video_id, v.video_title, v.video_headline, v.premium, v.video_url, v.video_text, v.video_date FROM videos v');
-
-    const comments = dbPromise('SELECT c.comment_id, c.comment_text, c.user_fk, c.video_fk, c.comment_date FROM comments c');
-
-    const blogs = dbPromise('SELECT b.blog_id, b.blog_title, b.blog_headline, b.blog_text, b.blog_text, b.blog_post_url FROM blogs b');
-
-    const images = dbPromise('SELECT i.image_id, i.blog_fk, i.image_url FROM images i');
-
-    Promise.all([users, videos, comments, blogs, images])
-        .then(data => cb(null, Object.assign(
-            {},
-            {users      : data[0]},
-            {videos     : data[1]},
-            {comments   : data[2]},
-            {blogs      : data[3]},
-            {images     : data[4]}
-        ))).catch(() => cb({error: 'db error'}));
+    Promise.all([getUsers, getVideos, getComments, getBlogs, getImages])
+        .then(data => {
+            const [users, videos, comments, blogs, images] = data;
+            cb(null, {users, videos, comments, blogs, images});
+        }).catch(() => cb({error: 'db error'}));
 };
 
 module.exports = getAdminData;
